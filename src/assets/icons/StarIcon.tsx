fc2ec3053ee8f95b6efbbac9c66483e223c51cf6
Maksim-Attsetski <maks2001maks2001@gmail.com>
2022-08-05 13:01:26 +0300

    Made burger menu

--- src/assets/icons/StarIcon.tsx
+++ src/assets/icons/StarIcon.tsx
@@ -5,7 +5,7 @@ const StarIcon: FC = () => {
         <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
                 d="M23.9986 5L17.8856 17.4776L4 19.4911L14.0589 29.3251L11.6544 43L23.9986 36.4192L36.3454 43L33.9586 29.3251L44 19.4911L30.1913 17.4776L23.9986 5Z"
-                fill="#fff" stroke="#fff" strokeWidth="4" strokeLinejoin="round"/>
+                fill="#c9083c" stroke="#c9083c" strokeWidth="4" strokeLinejoin="round"/>
         </svg>
     );
 };
--- src/components/MyHeader/MyHeader.tsx
+++ src/components/MyHeader/MyHeader.tsx
@@ -1,26 +1,44 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useMemo, useState} from 'react';
 import style from './MyHeader.module.scss';
 import {NavLink} from "react-router-dom";
 import {routeName} from "../Layout/routeName";
-import {Button} from "antd";
 import {useTypedDispatch, useTypedSelector} from "../../hooks/useRedux";
 import {changeTheme} from "../../redux/slices/themeSlice";
-
 
 const MyHeader: FC = () => {
     const {HOME, FILMS} = routeName
     const dispatch = useTypedDispatch()
+    const [burgerActive, setBurgerActive] = useState<boolean>(false)
     const {dark} = useTypedSelector(state => state.theme)
+
+    const burgerClass: string = useMemo(() =>
+        burgerActive ? `${style.headerBurger} ${style.active}` : `${style.headerBurger}`, [burgerActive])
+
+    const changeBurgerActive = (): void => {
+        burgerActive
+            ? document.body.classList.remove('overflow-hidden')
+            : document.body.classList.add('overflow-hidden')
+        setBurgerActive(!burgerActive)
+    }
+
+    window.addEventListener('resize', () => {
+        if(window.innerWidth < 640) return
+        setBurgerActive(false)
+    })
 
     return (
         <div className={style.header}>
             <div className={style.headerBody + ' container'}>
+                <div className={burgerClass} onClick={changeBurgerActive}/>
+
                 <div className={style.headerLinks}>
                     <NavLink className={style.headerLinks_link} to={HOME}>Home</NavLink>
                     <NavLink className={style.headerLinks_link} to={FILMS}>Films</NavLink>
                 </div>
+
                 <div className={style.theme}>
-                    <button className='btn' onClick={() => dispatch(changeTheme())}>{dark ? 'Dark' : 'Light'}</button>
+                    <button className='btn'
+                            onClick={() => dispatch(changeTheme())}>{dark ? 'Dark' : 'Light'}</button>
                 </div>
             </div>
         </div>
